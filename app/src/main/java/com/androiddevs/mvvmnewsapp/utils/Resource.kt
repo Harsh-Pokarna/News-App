9835b3457e87ac16726b74ed1620fb48c913faf9
Harsh Pokarna <f20200754@pilani.bits-pilani.ac.in>
2022-07-29 18:04:30 +0530

    architecture set up

--- app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Resource.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/utils/Resource.kt
@@ -6,6 +6,6 @@ sealed class Resource<T>( // Helpful to work with success, error and loading sta
 
 ) {
     class Success<T>(data: T?) : Resource<T>(data)
-    class Error<T>(error: String, data: T? = null): Resource<T(message = error, data = data)
+    class Error<T>(error: String, data: T? = null): Resource<T>(message = error, data = data)
     class Loading<T>: Resource<T>()
 }
