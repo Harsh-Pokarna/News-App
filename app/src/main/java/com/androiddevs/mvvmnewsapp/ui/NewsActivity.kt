151d1dfd72fd90e7d6e7b80631b1eafe09d0f061
Harsh Pokarna <f20200754@pilani.bits-pilani.ac.in>
2022-07-29 20:58:19 +0530

    testing git

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -19,6 +19,7 @@ class NewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_news)
 
         initialiseVariables()
